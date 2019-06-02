9a3fe2f6c93d2e8f16630dae69d2c2b96d2349ee
Michael Koslow <michael.koslow@gmail.com>
2019-06-02 14:27:19 -0500

    Create Initial Django App

---
+++ DJTest2/DJTest2/DJTest2/urls.py
@@ -0,0 +1,15 @@
+"""
+Definition of urls for DJTest2.
+"""
+
+from django.conf.urls import include, url
+import HelloDjangoApp.views
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+]
